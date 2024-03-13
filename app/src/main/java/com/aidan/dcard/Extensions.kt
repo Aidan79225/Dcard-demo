bf62e6d2d6622428a604da60b95eac28b83ac1bc
Aidan79225 <aidan79225@gmail.com>
2024-03-14 00:17:12 +0800

    [Feature] Add a search bar & snack bar for error msg (#5)

--- app/src/main/java/com/aidan/dcard/Extensions.kt
+++ app/src/main/java/com/aidan/dcard/Extensions.kt
@@ -1,8 +1,19 @@
 package com.aidan.dcard
 
 import android.app.Activity
+import android.content.Context
 import android.view.LayoutInflater
+
 
 fun <T> Activity.viewBinding(initializer: (inflater: LayoutInflater) -> T): Lazy<T> = lazy {
     initializer(this.layoutInflater)
+}
+
+fun getDensity(context: Context): Float {
+    val metrics = context.resources.displayMetrics
+    return metrics.density
+}
+
+fun Context.dp(dp: Float): Float {
+    return dp * getDensity(this)
 }
--- app/src/main/java/com/aidan/dcard/ui/MainActivity.kt
+++ app/src/main/java/com/aidan/dcard/ui/MainActivity.kt
@@ -2,14 +2,18 @@ package com.aidan.dcard.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
+import androidx.core.widget.addTextChangedListener
 import androidx.lifecycle.lifecycleScope
 import androidx.paging.LoadState
 import androidx.paging.Pager
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.aidan.dcard.R
 import com.aidan.dcard.databinding.ActivityMainBinding
 import com.aidan.dcard.entity.RepoInfo
 import com.aidan.dcard.viewBinding
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
@@ -26,7 +30,16 @@ class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(vb.root)
+        vb.svRepo.apply {
+            editText.setTextColor(ContextCompat.getColor(context, android.R.color.white))
+            editText.setHintTextColor(ContextCompat.getColor(context, R.color.text_dark_gray))
 
+            show()
+            editText.addTextChangedListener {
+                val searchKey = it?.takeIf { it.isNotEmpty() && it.isNotBlank() }?.toString() ?: return@addTextChangedListener
+                vm.startSearchRepo(searchKey)
+            }
+        }
         vb.rvRepoInfo.apply {
             adapter = repoInfoAdapter
             layoutManager = LinearLayoutManager(this@MainActivity)
@@ -40,10 +53,15 @@ class MainActivity: AppCompatActivity() {
         lifecycleScope.launch {
             repoInfoAdapter.loadStateFlow.collectLatest { loadStates ->
                 vb.pbLoading.isVisible = loadStates.append is LoadState.Loading || loadStates.refresh is LoadState.Loading
+                (loadStates.append as? LoadState.Error)?.also {
+                    Snackbar.make(vb.svRepo, it.error.message ?: "Something went wrong", Snackbar.LENGTH_SHORT).show()
+                }
+                (loadStates.refresh as? LoadState.Error)?.also {
+                    Snackbar.make(vb.svRepo, it.error.message ?: "Something went wrong", Snackbar.LENGTH_SHORT).show()
+                }
             }
         }
         vm.loadLanguageColor(this)
-        vm.fetch("q")
     }
 
     private fun subscribeNewPager(pager: Pager<Int, RepoInfo>) {
